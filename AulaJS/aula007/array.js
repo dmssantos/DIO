40fc0c729c6ca2e975d1bd5d2e0bddcca1daf2b7
dmssantos <45425587+dmssantos@users.noreply.github.com>
2020-09-02 15:07:13 -0300

    update

---
+++ AulaJS/aula007/array.js
@@ -0,0 +1,36 @@
+/*
+// Criar um array
+const arr = [1, 2, 3]
+const arr2 = new Array(1, 2, 3)
+
+// Array.of
+const persons = Array.of('John', 'Cris', 'Jenny')
+
+console.log(persons);
+
+const numbersAndStrings = Array.of(1, 2, 'strings', 'texto')
+
+console.log(numbersAndStrings);
+
+// Array
+const arr = Array(3)
+// [empt x 3] 3 espaços vazios
+
+const arr2 = Array(3, 2)
+// [3, 2]
+
+console.log(arr);
+
+// Array.from
+const divs = document.querySelectorAll('div')
+const arr = Array.from(divs)
+*/
+// push
+const frutas = ['melancia,', 'banana']
+
+console.log(frutas.push('laranja'));
+console.log(frutas);
+// pop
+console.log(frutas.pop());
+console.log(frutas);
+//
