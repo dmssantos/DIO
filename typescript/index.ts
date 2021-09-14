91efc631c8ffc461e3812a318bcb5a22c2e347ca
dmssantos <dms.santos_@hotmail.com>
2021-09-14 15:39:42 -0300

    update

--- typescript/index.ts
+++ typescript/index.ts
@@ -26,10 +26,57 @@ const animal: IDomestico = {
   tipo: 'terrestre',
 }
 */
-
+/*
 const input = document.getElementById('input') as HTMLInputElement;
 
 input.addEventListener('input', (event) => {
   const evt = event.currentTarget as HTMLInputElement;
   console.log(evt.value)
 })
+*/
+/*
+function adicionaApendiceALista<T>(array: T[], valor: T) {
+  return array.map(() => valor);
+}
+
+adicionaApendiceALista([1, 2, 3], 5);
+*/
+/*
+interface IUsuário {
+  id: string,
+  email: string,
+  cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+}
+
+
+function redirecione (usuario: IUsuário) {
+  if(usuario.cargo) {
+    //redirecionar (usuario.cargo)
+  }
+
+  //redirecionar para area de usuario
+}
+*/
+
+interface Cachorro {
+  readonly nome: string,
+  readonly idade: number,
+  readonly parqueFavorito?: string,
+}
+
+type CachorroSomenteLeitura = {
+  readonly [k in keyof Cachorro]-?: Cachorro[k];
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+  idade;
+  nome;
+  parqueFavorito;
+
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.nome = idade;
+  }
+}
+
+const cao = new MeuCachorro('Apollo', 14);
