dbe6b768c5864d750fde3955d91a7a34f527fd6f
dmssantos <dms.santos_@hotmail.com>
2022-02-03 10:52:34 -0300

    jwt

--- Inter-front-end-developer/node/first-server-node/src/routes/authorization.route.ts
+++ Inter-front-end-developer/node/first-server-node/src/routes/authorization.route.ts
@@ -2,20 +2,21 @@ import { Request, Response, NextFunction, Router } from "express";
 import ForbbidenError from "../models/errors/forbbiden.error.model";
 import userRepository from "../repositories/user.repository";
 import jwt from 'jsonwebtoken';
+import { StatusCodes } from "http-status-codes";
 
 const authorizationRoute = Router();
 
 authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const authorizationHeader = req.headers['authorization'];
-  
-    if(!authorizationHeader) {
+
+    if (!authorizationHeader) {
       throw new ForbbidenError('Credenciais não informadas');
     }
 
     const [authenticationType, token] = authorizationHeader.split(' ');
 
-    if(authenticationType !== 'Basic' || !token) {
+    if (authenticationType !== 'Basic' || !token) {
       throw new ForbbidenError('Tipo de Autenticação inválida');
     }
 
@@ -23,11 +24,22 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
 
     const [username, password] = tokenContent.split(":");
 
-    if(!username || !password) {
+    if (!username || !password) {
       throw new ForbbidenError('Credenciais não preenchidas');
     };
 
     const user = await userRepository.findByUsernameAndPassword(username, password);
+
+    if (!user) {
+      throw new ForbbidenError('Usuário ou senha inválidos');
+    }
+
+    const jwtPayload = { username: user.username }
+    const jwtOptions = { subject: user?.uuid }
+    const secretKey = 'my_secret_key'
+
+    const jwtToken = jwt.sign(jwtPayload, secretKey, jwtOptions)
+    res.status(StatusCodes.OK).json({ token: jwtToken })
   } catch (error) {
     next(error);
   }
